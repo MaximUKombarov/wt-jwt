199d577781ac916420727180fcf49fcd34c9d571
Maxim.U.Kombarov <maxim.u.kombarov@gmail.com>
2021-01-22 13:54:20 +0300

    Добавьте файлы проекта.

---
+++ wtJWT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace wt_jwt
+{
+    public class wtJWT
+    {
+        static public string GetLogin(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var tokenS = handler.ReadToken(token) as JwtSecurityToken;
+            return tokenS.Payload.ToString();
+        }
+    }
+}
